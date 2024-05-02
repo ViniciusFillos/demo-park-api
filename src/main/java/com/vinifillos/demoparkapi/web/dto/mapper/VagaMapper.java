ccfdcbf5a0bebb7965de7b225cd56ec406a6e83f
Vinicius Fillos <vinifillos@gmail.com>
2024-05-02 20:55:21 -0300

    Adicionando os DTOs e Mapper para Vagas: aula 95

---
+++ src/main/java/com/vinifillos/demoparkapi/web/dto/mapper/VagaMapper.java
@@ -0,0 +1,20 @@
+package com.vinifillos.demoparkapi.web.dto.mapper;
+
+import com.vinifillos.demoparkapi.entity.Vaga;
+import com.vinifillos.demoparkapi.web.dto.VagaCreateDto;
+import com.vinifillos.demoparkapi.web.dto.VagaResponseDto;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class VagaMapper {
+
+    private static Vaga toVaga(VagaCreateDto dto) {
+        return new ModelMapper().map(dto, Vaga.class);
+    }
+
+    private static VagaResponseDto toDto(Vaga vaga) {
+        return new ModelMapper().map(vaga, VagaResponseDto.class);
+    }
+}
