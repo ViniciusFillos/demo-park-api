42f1f7cd123b3159e350c34b1bd5d08e4e59b4e4
Vinicius Fillos <vinifillos@gmail.com>
2024-05-04 00:43:01 -0300

    Realizando o check-in de um veículo via postman: aula 108

--- src/main/java/com/vinifillos/demoparkapi/web/dto/EstacionamentoCreateDto.java
+++ src/main/java/com/vinifillos/demoparkapi/web/dto/EstacionamentoCreateDto.java
@@ -17,7 +17,7 @@ public class EstacionamentoCreateDto {
 
     @NotBlank
     @Size(min = 8, max = 8)
-    @Pattern(regexp = "[A-Z]{3}-[0-9]]{4}", message = "A placa do veículo deve seguir o padrão 'XXX-0000'")
+    @Pattern(regexp = "[A-z]{3}-\\d[A-j0-9]\\d{2}", message = "A placa do veículo deve seguir o padrão 'XXX-0X00'")
     private String placa;
     @NotBlank
     private String marca;
