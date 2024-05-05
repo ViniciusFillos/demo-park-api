b2c5a5b18fbb1512cf83be443ececd90633645d2
Vinicius Fillos <vinifillos@gmail.com>
2024-05-05 05:38:30 -0300

    Adicionando o recurso para localizar os dados de check-in pelo recibo: aula 112

--- src/main/java/com/vinifillos/demoparkapi/service/ClienteVagaService.java
+++ src/main/java/com/vinifillos/demoparkapi/service/ClienteVagaService.java
@@ -2,9 +2,11 @@ package com.vinifillos.demoparkapi.service;
 
 
 import com.vinifillos.demoparkapi.entity.ClienteVaga;
+import com.vinifillos.demoparkapi.exception.EntityNotFoundException;
 import com.vinifillos.demoparkapi.repository.ClienteVagaRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.ReadOnlyProperty;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,5 +18,13 @@ public class ClienteVagaService {
     @Transactional
     public ClienteVaga salvar(ClienteVaga clienteVaga) {
         return clienteVagaRepository.save(clienteVaga);
+    }
+
+    @Transactional
+    @ReadOnlyProperty
+    public ClienteVaga buscarPorRecibo(String recibo) {
+            return clienteVagaRepository.findByReciboAndDataSaidaIsNull(recibo).orElseThrow(
+                    () -> new EntityNotFoundException(String.format("Recibo %s não encontrado no sistema ou check-out já realizado", recibo))
+            );
     }
 }
--- src/main/java/com/vinifillos/demoparkapi/web/controller/EstacionamentoController.java
+++ src/main/java/com/vinifillos/demoparkapi/web/controller/EstacionamentoController.java
@@ -1,6 +1,7 @@
 package com.vinifillos.demoparkapi.web.controller;
 
 import com.vinifillos.demoparkapi.entity.ClienteVaga;
+import com.vinifillos.demoparkapi.service.ClienteVagaService;
 import com.vinifillos.demoparkapi.service.EstacionamentoService;
 import com.vinifillos.demoparkapi.web.dto.EstacionamentoCreateDto;
 import com.vinifillos.demoparkapi.web.dto.EstacionamentoResponseDto;
@@ -18,10 +19,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -33,6 +31,7 @@ import java.net.URI;
 public class EstacionamentoController {
 
     private final EstacionamentoService estacionamentoService;
+    private final ClienteVagaService clienteVagaService;
 
     @Operation(summary = "Operação de check-in", description = "Recurso para dar entrada de um veículo no estacionamento." +
             " Requisição exige um uso de bearer token. Acesso restrito a Role='ADMIN'",
@@ -65,5 +64,13 @@ public class EstacionamentoController {
                 .toUri();
         return ResponseEntity.created(location).body(responseDto);
 
+    }
+
+    @GetMapping("/check-in/{recibo}")
+    @PreAuthorize("hasAnyRole('ADMIN','CLIENT')")
+    public ResponseEntity<EstacionamentoResponseDto> getByRecibo(@PathVariable String recibo) {
+        ClienteVaga clienteVaga = clienteVagaService.buscarPorRecibo(recibo);
+        EstacionamentoResponseDto dto = ClienteVagaMapper.toDto(clienteVaga);
+        return ResponseEntity.ok(dto);
     }
 }
